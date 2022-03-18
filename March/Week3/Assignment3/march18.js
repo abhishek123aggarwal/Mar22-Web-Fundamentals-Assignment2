b7af6ad6b09d3c7f80794c912dace81f88bc8b7e
abhishek <abhishek@gmail.com>
2022-03-18 21:35:40 +0530

    Assignmemt 3 is puched

---
+++ March/Week3/Assignment3/march18.js
@@ -0,0 +1,26 @@
+/*******************************************************
+ * Question 1 : Program to write the first 10 numbers.
+ ******************************************************/
+
+function first10Numbers() {
+  for (let i = 0; i < 10; i++) {
+    console.log(i)
+  }
+}
+
+
+first10Numbers();
+
+/*******************************************************
+ * Question 2 : Program to write the first 20 numbers.
+ ******************************************************/
+
+function first20Numbers() {
+  for (let i = 0; i < 20; i++) {
+    console.log(i);
+  }
+}
+
+first20Numbers();
+
+
